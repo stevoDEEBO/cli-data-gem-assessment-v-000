08ff070254768f434885e3d849afa8ec00a02fac
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-07-18 16:12:50 +0000

    automatically backed up by learn

--- whisky-picker/lib/whisky/picker.rb
+++
@@ -1,7 +0,0 @@
-require "whisky/picker/version"
-
-module Whisky
-  module Picker
-    # Your code goes here...
-  end
-end
--- whisky-picker/lib/whisky/picker/version.rb
+++
@@ -1,5 +0,0 @@
-module Whisky
-  module Picker
-    VERSION = "0.1.0"
-  end
-end
